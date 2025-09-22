7b17eccafc405bef0dda19f39449645e47f10389
karlosDuran <ferni0425@gmail.com>
2025-09-21 19:56:22 -0600

    feat(admin): add admin layout with Flowbite sidebar, navigation includes, and dashboard slot

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,3 @@
+<x-admin-layout>
+    hola desde tu casa xdd
+</x-admin-layout>
--- routes/admin.php
+++ routes/admin.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function (){
-    return 'que rollito d';
-});
+    return view('admin.dashboard');
+})->name('dashboard');
